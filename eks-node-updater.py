089ed8769a7db9d34f49a4bf338d822fdcd0ca79
Matthew Garber <matthew.garber@contino.io>
2020-03-17 14:00:22 -0400

    add function to query node metadata from k8s client

--- eks-node-updater.py
+++ eks-node-updater.py
@@ -12,6 +12,9 @@ Imports
 import boto3
 import argparse
 import time
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
+import subprocess
 
 """
 Globals
@@ -21,7 +24,7 @@ Globals
 Client Connection
 """
 
-client = boto3.client('eks')
+eks_client = boto3.client('eks')
 
 """
 # Define CLI arguments:
@@ -42,8 +45,10 @@ args = parser.parse_args()
 """
 
 def main():
+    print ("Querying Node Data before Update...")
+    query_nodes()
     # Add conditional error logic in event cluster doesn't exit/wrap in try&except
-    cluster_query = client.describe_cluster(
+    cluster_query = eks_client.describe_cluster(
                 name=args.cluster_name
                 )
     eks_version = cluster_query['cluster']['version']
@@ -55,12 +60,12 @@ def main():
     else:
         print ("The EKS Version supports Managed Node Groups. Querying Managed Nodes...")
 
-    mng = client.list_nodegroups(clusterName=args.cluster_name)
+    mng = eks_client.list_nodegroups(clusterName=args.cluster_name)
     # Modify to support handling more than a single NG - We're making assumptions of a single NG per cluster.
     node_groups = mng['nodegroups']
     for node_group in node_groups:
         print ("EKS Cluster: " + args.cluster_name + " is leveraging managed node group: " + node_group)
-        node_metadata = client.describe_nodegroup(clusterName=args.cluster_name, nodegroupName=node_group)
+        node_metadata = eks_client.describe_nodegroup(clusterName=args.cluster_name, nodegroupName=node_group)
         node_ami_version = node_metadata['nodegroup']['releaseVersion']
         node_eks_version = node_metadata['nodegroup']['version']
         print ("EKS Managed Node Group: " + node_group + " Is currently running AMI: " + node_ami_version + " and on EKS Version: " + node_eks_version)
@@ -71,7 +76,7 @@ Query Available Updates for a respective Managed Node Group
 """
 
 def node_update(node_group, eks_version, node_ami_version):
-    node_update = client.update_nodegroup_version(clusterName=args.cluster_name, nodegroupName=node_group, version=eks_version, force=args.force_update)
+    node_update = eks_client.update_nodegroup_version(clusterName=args.cluster_name, nodegroupName=node_group, version=eks_version, force=args.force_update)
     starttime = time.time()
     updated_node_version = node_update['update']['params'][0]['value']
     updated_node_ami = node_update['update']['params'][1]['value']
@@ -89,7 +94,7 @@ Execute Node Updates
 """
 
 def node_update_check(node_group, node_update_id, starttime):
-    update_check = client.describe_update(name=args.cluster_name, updateId=node_update_id, nodegroupName=node_group)
+    update_check = eks_client.describe_update(name=args.cluster_name, updateId=node_update_id, nodegroupName=node_group)
     if update_check['update']['status'] == 'InProgress':
         print ("Still In Progress...")
         time.sleep(30)
@@ -97,9 +102,27 @@ def node_update_check(node_group, node_update_id, starttime):
     elif update_check['update']['status'] == 'Successful':
         endtime = time.time()
         print ("Update Complete. Total Duration: " + str(endtime-starttime) + " Seconds")
+        query_nodes()
     elif update_check['update']['status'] == 'Failed' or 'Cancelled':
         print ("Update Failed or Cancelled: " + str(update_check['update']['errors']))
         exit(1)
-    
+"""
+Connect to EKS Cluster and Query Node Information
+"""
+
+def query_nodes():
+    get_kube_config()
+    cfg = config.load_kube_config()
+    api_instance = client.CoreV1Api(cfg)
+    node_data = api_instance.list_node(pretty='true')
+    for data in node_data.items:
+        print ("Node: " + data.metadata.name + " Is running version: " + data.status.node_info.kubelet_version)
+"""
+Leverage AWS CLI to Auth to EKS Cluster for K8S Client
+"""
+def get_kube_config():
+    clicall = 'aws eks update-kubeconfig --name '+args.cluster_name
+    kube_config = subprocess.run([clicall], shell=True, check=True)
+
 if __name__== "__main__":
   main()
